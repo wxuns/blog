078965f5d8e63a79b4a346f000b3bdef3ddfc27b
wxuns <php7ml@gmail.com>
2019-05-08 18:14:58 +0800

    article

--- app/modules/Adminajcn/controllers/Article.php
+++ app/modules/Adminajcn/controllers/Article.php
@@ -7,13 +7,19 @@ class ArticleController extends BaseController
         $this->getView()->display('admin/article/list');
         return false;
     }
+    public function articleclassAction()
+    {
+        $this->getView()->display('admin/article/classlist');
+        return false;
+    }
     public function addAction()
     {
         $this->getView()->display('admin/article/addarticle');
         return false;
     }
-    public function articleclassAction()
+    public function addclassAction()
     {
+        $this->getView()->display('admin/article/addclass');
         return false;
     }
 }
